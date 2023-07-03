26b092688741535248fba8449dfe830cd7c1bd4b
samim hossain sujon <samimhossainsanto@gmail.com>
2023-07-03 10:53:54 +0600

    requre done

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -21,8 +21,7 @@ const Contact = () => {
             .then(
                 (result) => {
                     Swal.fire(
-                        'Good job!',
-                        'You clicked the button!',
+                        'Your send successfully',                       
                         'success'
                     );
                     form.current.reset(); // Reset the form
@@ -37,12 +36,12 @@ const Contact = () => {
         <StyledContactForm >
             <form ref={form} onSubmit={sendEmail}>
                 <label>Name</label>
-                <input type="text" name="user_name" />
+                <input type="text" name="user_name" required />
                 <label>Email</label>
-                <input type="email" name="user_email" />
+                <input type="email" name="user_email" required />
                 <label>Message</label>
                 <textarea name="message" />
-                <input type="submit" value="Send" />
+                <input type="submit" value="Send" required />
             </form>
         </StyledContactForm>
     );
